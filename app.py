175eaaf830bbca2ec642e54699ae5bfa7bb990b5
Franzo <99118328+franzo9448@users.noreply.github.com>
2023-02-17 11:43:35 +0000

    added callisto religion

--- app.py
+++ app.py
@@ -10,6 +10,39 @@ def home():
 def nozarik():
   return render_template("nozarik.html") 
 
+@app.route("/politica")
+def politica():
+  return render_template("politics.html") 
+
+@app.route("/floreal")
+def floreal():
+  return render_template("floreal.html") 
+
+@app.route("/nalowand")
+def nalowand():
+  return render_template("nalowand.html")
+
+@app.route("/pentola")
+def pentola():
+  return render_template("pentola.html") 
+
+@app.route("/callisto")
+def callisto():
+  return render_template("callisto.html") 
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+@app.route("/characters")
+def characters():
+  return render_template("characters.html")
+
+@app.route("/notes")
+def notes():
+  return render_template("notes.html")
+
+
 if __name__ == "__main__":
   print("dentro id")
   app.run(host = "0.0.0.0",debug =True )
